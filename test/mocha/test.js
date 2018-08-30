595b084e382616a1d77283601c6d3f6cf0a52044
Jayden Liang <jliang01@fortinet.com>
2018-08-29 18:03:58 -0700

    Initial commit. Add all neccessary files to this repository
    Change-Id: I13f299444bec0ffbe448b6217a19d03e3b2521c8

---
+++ test/mocha/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Dummy test', function() {
+    describe('Array.indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
