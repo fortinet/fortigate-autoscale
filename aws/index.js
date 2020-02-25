d9e2e1f1a2299572fd14ae1d21b3b0f7b71b2f22
Jayden Liang <jliang01@fortinet.com>
2020-02-24 17:15:33 -0800

    Revert "fixed deleting AutoscaleRole tag from an empty array of instances"
    This reverts commit bbd2dd052dd6ceee5606accfd47838db69cb78ce.

--- aws/index.js
+++ aws/index.js
@@ -1858,28 +1858,24 @@ class AwsPlatform extends AutoScaleCore.CloudPlatform {
         };
         try {
             const instances = await ec2.describeInstances(params).promise();
-            let taggedInstances = [];
+            let masterInstances = [];
             instances.Reservations.forEach(resv => {
                 const resvInstances = resv.Instances.filter(resvInstance => {
                     // return true if it contains a tag key: AutoscaleRole and value: master
                     return resvInstance.Tags.filter(tag =>
                         tag.Key === 'AutoscaleRole' && tag.Value === 'master').length > 0;
                 });
-                taggedInstances = [...taggedInstances, ...resvInstances];
+                masterInstances = [...masterInstances, ...resvInstances];
             });
-
-            // remove the master tag from existing instances only if the tag exists.
-            if (taggedInstances.length > 0) {
-                let deleteParams = { Resources: [], Tags: [tagMaster] };
-                taggedInstances.forEach(masterInstance => {
-                    deleteParams.Resources.push(masterInstance.InstanceId);
-                });
-                await ec2.deleteTags(deleteParams).promise();
-                logger.info(`AutoscaleRole is removed from ${taggedInstances.length} instance(s).`);
-            }
+            logger.log(JSON.stringify(masterInstances));
+            // remove the master tag from existing instances
+            let deleteParams = { Resources: [], Tags: [tagMaster]};
+            masterInstances.forEach(masterInstance => {
+                deleteParams.Resources.push(masterInstance.InstanceId);
+            });
+            await ec2.deleteTags(deleteParams).promise();
             // add the master tag to the one with id === masterInstanceId
-            await ec2.createTags({ Resources: [masterInstanceId], Tags: [tagMaster] }).promise();
-            logger.info(`AutoscaleRole is added to instance (id: ${masterInstanceId}).`);
+            await ec2.createTags({ Resources: [masterInstanceId], Tags: [tagMaster]}).promise();
             logger.info('called updateHAAPRoleTag');
             return true;
         } catch (error) {
