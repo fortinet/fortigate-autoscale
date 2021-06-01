0490b4dfa610fa3f36c1506d15b869a66f398388
Jayden Liang <jliang01@fortinet.com>
2021-06-01 13:36:32 -0700

    remove all contents from the main branch

--- core/setting-items/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-/*
-A collection of setting items.
-Author: Fortinet
-*/
-exports.FortiAnalyzerSettingItem = require('./forti-analyzer-setting-item');
--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-/*
-FortiGate Autoscale Main (1.0.0)
-Author: Fortinet
-*/
-
